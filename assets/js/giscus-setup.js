daf2920261dad0e6e3117fb021503abd952f0f00
fargocheungyc-jpg <fargo.cheungyc@gmail.com>
2026-05-22 05:13:25 +0000

    Deploying to gh-pages from @ fargocheungyc-jpg/fargocheungyc-jpg@16d80a8dedc5a52b89f2edf8b106ab710bf76e71 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "fargocheungyc-jpg/alfargocheung-jpg",
+    "data-repo": "fargocheungyc-jpg/fargocheungyc-jpg",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
